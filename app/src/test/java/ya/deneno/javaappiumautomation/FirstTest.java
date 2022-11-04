31a00010c2c9feaa6bd9755b80defdaec77f690c
denenoLimex <d.maksimov@limehd.tv>
2022-11-04 14:15:49 +0300

    Ex7 пройдено

--- app/src/test/java/ya/deneno/javaappiumautomation/FirstTest.java
+++ app/src/test/java/ya/deneno/javaappiumautomation/FirstTest.java
@@ -33,6 +33,9 @@ public class FirstTest {
         capabilities.setCapability("appActivity", ".main.MainActivity");
         capabilities.setCapability("app", "C:/Users/Deneno/Desktop/JavaAppiumAutomation/app/apks/org.wikipedia.apk");
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        if (driver.getOrientation() != ScreenOrientation.PORTRAIT) {
+            driver.rotate(ScreenOrientation.PORTRAIT);
+        }
     }
 
     @After
