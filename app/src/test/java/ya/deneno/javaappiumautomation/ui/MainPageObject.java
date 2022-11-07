f9ec520e01865e245660802db440a913878c5986
denenoLimex <d.maksimov@limehd.tv>
2022-11-07 18:39:21 +0300

    Все-таки с 2 секундами стабильно

--- app/src/test/java/ya/deneno/javaappiumautomation/ui/MainPageObject.java
+++ app/src/test/java/ya/deneno/javaappiumautomation/ui/MainPageObject.java
@@ -123,7 +123,7 @@ public class MainPageObject {
 
     public void sleep() {
         try {
-            Thread.sleep(1000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
